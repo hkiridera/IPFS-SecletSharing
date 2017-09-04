438ca30e89f75ea3cff80ed25ea31b8cae0f100d
hkiridera <migratory39@gmail.com>
2017-09-04 13:32:20 +0000

    test用phpもresponseを表示するように

--- upload.php
+++ upload.php
@@ -4,22 +4,20 @@
 $url = 'http://192.168.12.110:8000/upload';
 // 送信するデータ
 $filename = 'sample.txt';
-$image = file_get_contents('cc2.jpg');
+$file = file_get_contents('sample.txt');
 // POSTするデータを作成
 $header = [
     "Content-Type: application/json; charset=UTF-8;",
-    "Content-Length: ".strlen($image),
+    "Content-Length: ".strlen($file),
 ];
 $context = stream_context_create([
     'http' => [
         'method'=> 'POST',
         'header'=> implode("\r\n", $header),
-        'content' => $image
+        'content' => $file
     ]
 ]);
 $raw_data = file_get_contents($url, false, $context);
-// jsonからstdClassに変換
-$data = json_decode($raw_data);
 // 結果を表示
-echo $data->message . PHP_EOL;
+echo $raw_data;
 ?>
