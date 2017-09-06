031d1107f1ac7840baea06e11a54c2b1e227c295
hkiridera <migratory39@gmail.com>
2017-09-06 19:57:30 +0900

    復号化の試験

---
+++ download.php
@@ -0,0 +1,23 @@
+
+<?php
+// file_get_contentsでデータを取得
+$url = 'http://192.168.12.110:8000/download';
+// 送信するデータ
+$filename = '0217ad03124e41fc9c8f6e7830bfd7fb.metadata';
+$file = file_get_contents('0217ad03124e41fc9c8f6e7830bfd7fb.metadata');
+// POSTするデータを作成
+$header = [
+    "Content-Type: application/json; charset=UTF-8;",
+    "Content-Length: ".strlen($file),
+];
+$context = stream_context_create([
+    'http' => [
+        'method'=> 'POST',
+        'header'=> implode("\r\n", $header),
+        'content' => $file
+    ]
+]);
+$raw_data = file_get_contents($url, false, $context);
+// 結果を表示
+echo $raw_data;
+?>
