200fd07a1b44a2d13b7bc0bc2fa52b337583dffd
hkiridera <migratory39@gmail.com>
2017-09-09 08:50:08 +0900

    metadataのファイル名変更

--- test/download.py
+++ test/download.py
@@ -5,7 +5,7 @@ from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 import urllib2
 
-filename = "test.txt"
+filename = "../metadata/1b89fba8602cb7ee34b90df361e1d9f9.metadata"
 
 if __name__ == '__main__':
     # Register streaming http handlers to urllib2 global object
